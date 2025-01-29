32b95d67c490aa25821849770e1f1cbca21e521f
BiplawSingh <biplawgaurav@gmail.com>
2025-01-29 12:00:41 +0530

    Design changes

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 const publicationButton = document.querySelector(".dropdown .fButton");
 const publicationDropdown = document.querySelector(".dropdown-content");
-
-publicationButton.addEventListener("click", () => {
-    publicationDropdown.classList.toggle("show");
-});
-
+    
 
 const studentButton = document.querySelectorAll(".dropdown .fButton")[1];
 const studentDropdown = document.querySelectorAll(".dropdown-content")[1];
-
-studentButton.addEventListener("click", () => {
-    studentDropdown.classList.toggle("show");
-});
 
 
 window.addEventListener("click", (e) => {
